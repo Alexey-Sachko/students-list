f0f3978b936356eb64a8827a9d37f2589147386b
Alexey Sachko <alexey.sachko@gmail.com>
2020-07-28 15:36:04 +0500

    removed extra controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentsModule } from './students/students.module';
 import { config } from './config';
 
 @Module({
   imports: [StudentsModule, TypeOrmModule.forRoot(config.dbConfig)],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
