bb58cbf93881ee731dccb5e2b0a2dbbc47661f91
Alexey Sachko <alexey.sachko@gmail.com>
2020-07-30 08:34:27 +0500

    allowed cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({ origin: ['http://localhost:3000'] });
+
   const options = new DocumentBuilder()
     .setTitle('Students example')
     .setDescription('The students API description')
